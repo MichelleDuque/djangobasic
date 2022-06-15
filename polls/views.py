409978bc3bbd8837eccc85f434466c1b5c7e032a
MichelleDuque <michelled_1994@hotmail.com>
2022-06-15 12:37:40 -0500

    Creacion de views y urls en polls

---
+++ polls/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Estás en la pagina principal de Premios Baloa")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+
