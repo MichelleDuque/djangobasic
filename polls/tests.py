c476040f383f656114a4ca451f755183e00b1b83
MichelleDuque <michelled_1994@hotmail.com>
2022-06-30 13:21:01 -0500

    Se crearon los archivos de css

--- polls/tests.py
+++ polls/tests.py
@@ -49,6 +49,11 @@ def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
+def test_question_wo_answers(self):
+    question = Question(question_text="test", pub_date=timezone.now())
+    with self.assertRaises(Exception):
+        question.save()
+
 class QuestionIndexViewTests(TestCase):
 
     def test_no_questions(self):
@@ -122,4 +127,37 @@ class QuestionDetailViewTest(TestCase):
         url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+
+class QuestionResultViewTests(TestCase):
+    def test_no_question(self):
+        """
+        The result view of a question that doesn't exist
+        return a 404 error not found
+        """
+        url = reverse("polls:results", args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_future_question(self):
+        """
+        The result view of a question with a pub_date in the future
+        return a 404 error not found
+        """
+        future_question = create_question(question_text="¿Quien es el mejor CD de Platzi?", days=30)
+        url = reverse("polls:results", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The result view of a question with a pub_date in the past
+        displays the question's text
+        """
+        past_question = create_question(question_text="Past question", days=-30)
+        url = reverse("polls:results", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
 
--- polls/views.py
+++ polls/views.py
@@ -51,6 +51,14 @@ class ResultView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
+    def get_queryset(self):
+        """
+        Exclude any question that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+    
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -64,6 +72,8 @@ def vote(request, question_id):
         selected_choice.votes +=1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
 
 
 
